d68442df616070cd930b2bc9f6e1cb8245f17b2d
n4sm <hobbyste.sda@gmail.com>
2022-02-27 21:58:29 +0100

    I did bad things

--- src/core_mapper.c
+++ src/core_mapper.c
@@ -50,32 +50,24 @@ mdata_binary_t* load_interp(Elf64_Phdr* s_ph, mdata_binary_t* s_binary)
 int map_load(Elf64_Phdr* s_ph, mdata_binary_t* s_binary) 
 {
     uint64_t curr_map = 0x0;
-    uint64_t sz = PAGE_ROUND((PAGE_ROUND((curr_map + s_ph->p_filesz))));
+    uint64_t sz = PAGE_ROUND((PAGE_ROUND((curr_map + s_ph->p_filesz)))) + 1;
 
     if (!s_binary->base) {
         if (MAP_FAILED == (s_binary->base = (uint8_t* )mmap((void* )(s_binary->pie ? base_address() : s_ph->p_vaddr), sz, PROT_READ | PROT_WRITE | _PROT_EXEC(s_ph->p_flags), MAP_FIXED | MAP_PRIVATE, s_binary->fd, PAGE_ALIGN(s_ph->p_offset)))) {
             return -1;
         }
-        // list_add_map(s_binary, 
-        //         PROT_READ | _PROT_EXEC(s_ph->p_flags) | _PROT_WRITE(s_ph->p_flags),
-        //         (uint64_t)s_binary->base,
-        //         sz + 1); // fff + 1
     } else {
         curr_map = (uint64_t)(s_binary->pie ? (uint64_t)s_binary->base + s_ph->p_vaddr : (uint64_t)s_ph->p_vaddr);
-        if (MAP_FAILED == mmap((void *)PAGE_ALIGN(curr_map), PAGE_ROUND((PAGE_ROUND((curr_map + s_ph->p_filesz)) - PAGE_ALIGN(curr_map))), PROT_READ | PROT_WRITE | _PROT_EXEC(s_ph->p_flags), MAP_FIXED | MAP_PRIVATE, s_binary->fd, PAGE_ALIGN(s_ph->p_offset))) {
+        if (MAP_FAILED == mmap((void *)PAGE_ALIGN(curr_map), PAGE_ROUND((PAGE_ROUND((curr_map + s_ph->p_filesz)) - PAGE_ALIGN(curr_map))) + 1, PROT_READ | PROT_WRITE | _PROT_EXEC(s_ph->p_flags), MAP_FIXED | MAP_PRIVATE, s_binary->fd, PAGE_ALIGN(s_ph->p_offset))) {
             return -1;
         }
-        // list_add_map(s_binary,
-        //         PROT_READ | _PROT_EXEC(s_ph->p_flags) | _PROT_WRITE(s_ph->p_flags),
-        //         PAGE_ALIGN(curr_map),
-        //         sz + 1); // fff + 1
     }
 
     if (DEBUG & LOG_MAP) fprintf(s_binary->debug_stream, 
                                 "[>] %lx - %lx %lx\n", 
                                 (uint64_t)(curr_map ? PAGE_ALIGN(curr_map) : (uint64_t)s_binary->base), 
                                 (uint64_t)((curr_map ? PAGE_ALIGN(curr_map) : (uint64_t)(s_binary->base)) + sz), 
-                                sz + 1); // fff + 1
+                                sz);
 
     if (s_ph->p_memsz > s_ph->p_filesz && curr_map) {
         uint64_t map_filesz = curr_map + s_ph->p_filesz;
@@ -84,11 +76,11 @@ int map_load(Elf64_Phdr* s_ph, mdata_binary_t* s_binary)
         uint64_t bss_end = PAGE_ROUND((curr_map + s_ph->p_memsz));
 
         memset((void* )map_filesz,
-               0x0,
-               map_end - map_filesz);
+                        0x0,
+                        map_end - map_filesz);
 
         if (bss_end > map_end) {
-            if (MAP_FAILED == mmap((void *)map_end+1, PAGE_ROUND((bss_end - map_end)) + 1, PROT_READ | _PROT_WRITE(s_ph->p_flags) | _PROT_EXEC(s_ph->p_flags), MAP_ANONYMOUS | MAP_FIXED | MAP_PRIVATE, -1, 0x0)) {
+            if (MAP_FAILED == mmap((void *)map_end+1, PAGE_ROUND((bss_end - map_end)), PROT_READ | _PROT_WRITE(s_ph->p_flags) | _PROT_EXEC(s_ph->p_flags), MAP_ANONYMOUS | MAP_FIXED | MAP_PRIVATE, -1, 0x0)) {
                 return -1;
             }
 
@@ -114,8 +106,6 @@ int map_load(Elf64_Phdr* s_ph, mdata_binary_t* s_binary)
             }
         }
     }
-
-
 
     // set the right permissions
     if (mprotect(curr_map ? (void *)PAGE_ALIGN(curr_map) : (void* )s_binary->base, PAGE_ROUND(s_ph->p_memsz), PROT_READ | _PROT_EXEC(s_ph->p_flags) | _PROT_WRITE(s_ph->p_flags) | _PROT_EXEC(s_ph->p_flags))) {
